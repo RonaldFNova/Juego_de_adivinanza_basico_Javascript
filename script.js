05b4a77680348c42706b162c1757b8e01af7168a
Ronald Bermudez <ronaldstevenrios@gmail.com>
2025-08-11 09:37:51 -0500

    Agregando un historial de los numeros ingresados en los intentos

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let numeroAleatorio;
 let intentos = [];
 let botonReset;
-let intentosCantidad
-let numero
-let p
+let intentosCantidad;
+let numero;
+let p;
 let cantidadBoton = true;
+let historial;
 
 document.addEventListener("DOMContentLoaded", function (){
     numeroAleatorio = Math.trunc(Math.random() * 100);
-})
+    historial = document.createElement("p");
+    document.getElementById("Historial").appendChild(historial);
+    p = document.getElementById("Mensaje");
+    
+});
 
 function ValoresPorDefecto(){
     botonReset = document.createElement("button");
@@ -16,7 +21,6 @@ function ValoresPorDefecto(){
     intentosCantidad = document.createElement("p");
     console.log(numeroAleatorio);
     numero = document.getElementById("numeroEntrada").value;
-    p = document.getElementById("Mensaje");
 }
 
 function Comprobar()
@@ -26,22 +30,36 @@ function Comprobar()
     }
 
     if (numero == ""){
-        p.textContent = "numero incorrecto intente con otro";
+        p.textContent = "numero invalido intente con otro";
+        p.style.color = "red";
+    }
+    else if(numero > 100 || numero <= 0)
+    {
+        p.textContent ="El numero excede los valores permitidos"
+        p.style.color = "black";
     }
     else if (numero > numeroAleatorio){
-        p.textContent = "numero es mas pequeño";
+        p.textContent = "El numero es mas pequeño";
+        p.style.color = "red";
         intentos.push(numero);
+        historial.textContent = "Numeros ingresados: " + intentos;
     }
     else if (numero < numeroAleatorio) {
-        p.textContent = "numero es mas grande";
+        p.textContent = "El numero es mas grande";
+        p.style.color = "red";
         intentos.push(numero);
+        historial.textContent = "Numeros ingresados: " + intentos;
+
     }
     else{
         p.textContent = "numero correcto";
+        p.style.color = "green";
         intentosCantidad.textContent = "el total de intentos que te tomó fue de : " + (intentos.length + 1);
-        document.getElementById("Mensaje").appendChild(intentosCantidad)
+        intentosCantidad.style.color = "green";
+        document.getElementById("Mensaje").appendChild(intentosCantidad);
         document.getElementById("prueba").appendChild(botonReset);
-        cantidadBoton = false
+        historial.textContent = "Numeros ingresados: " + intentos;
+        cantidadBoton = false;
     }
     
     botonReset.addEventListener("click", function(){
@@ -50,7 +68,8 @@ function Comprobar()
         p.textContent = "";
         intentosCantidad.remove();
         cantidadBoton = true;
-    })
-    
+        intentos.length = 0;
+        historial.textContent = "";
+    });
 
 }
